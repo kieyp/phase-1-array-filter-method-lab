a64114192e3f47c4b5245a2547411f3966e57340
Boniface <Cheruiyotbony@gmail.com>
2023-10-10 09:40:30 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Code your solution here
+
+function findMatching(drivers,name)
+
+{
+  const themate=drivers.filter((driver)=>
+  
+  {
+    return driver.toLowerCase()===name.toLowerCase()
+  })
+  return themate
+}
+
+
+function fuzzyMatch(drivers,name)
+
+{
+  const lettersMatching=drivers.filter((driver)=>
+  
+  {
+    return driver.slice(0,name.length)===name
+  })
+  return lettersMatching
+}
+
+
+
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+
+function matchName(drivers,name)
+{
+const matchedDrivers=drivers.filter((driver)=>
+{
+  return driver.name==='Bobby'
+})
+return matchedDrivers
+}
